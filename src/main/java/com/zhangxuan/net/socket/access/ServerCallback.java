5767a4b7a1a87f86d9ef0adaf7e0bbb9b0ff4c7d
黄显 <454430812@qq.com>
2020-02-15 19:21:05 +0800

    删除文件 src

--- src/main/java/com/zhangxuan/net/socket/access/ServerCallback.java
+++
@@ -1,9 +0,0 @@
-package com.zhangxuan.net.socket.access;
-
-
-
-public interface ServerCallback extends ClientCallback {
-	void onStarted(Server server);
-	void onStoped(Server server);
-	void onAccepted(Client client);
-}
--- src/main/java/com/zhangxuan/net/socket/model/AckFrame.java
+++
@@ -1,5 +0,0 @@
-package com.zhangxuan.net.socket.model;
-
-public class AckFrame extends PingFrame {
-
-}
